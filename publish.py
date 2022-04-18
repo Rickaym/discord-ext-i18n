5115082aaaef4b7178a22c6aee1268f981ff1e26
Ricky <psmtitant@gmail.com>
2022-04-18 10:40:56 +0630

    reframe ignore

--- publish.py
+++ publish.py
@@ -1,11 +1,15 @@
+"""
+Automatically publishing to pypi.
+"""
+
 import wexpect
 
 def main():
     setupfile = "setup.py"
     child = wexpect.spawn("py", args=[setupfile, "sdist"])
     print(child.read())
-    child = wexpect.spawn("twine", args=["upload", "dist/*"])
-    print(child.read())
+    #child = wexpect.spawn("twine", args=["upload", "dist/*"])
+    #print(child.read())
 
 if __name__ == "__main__":
     main()
